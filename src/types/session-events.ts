d73e276480aeb002eb0e08ed0afe872c2be69370
rjoshi <raghavjoshi789@gmail.com>
2025-04-15 01:06:17 +0530

    Basic setup for classroom websocket events

---
+++ src/types/session-events.ts
@@ -0,0 +1,23 @@
+export enum SessionEmitEventNames {
+  Join = "join-classroom",
+  Leave = "leave-classroom",
+  UpdateScore = "update-score",
+}
+
+export enum SessionHandleEventNames {
+  UserJoined = "user-joined",
+  UserLeft = "user-left",
+  ScoreUpdated = "score-updated",
+}
+
+export type SessionData = {
+  userId: string;
+  username: string;
+  scores: SessionScoreData;
+};
+
+export type SessionScoreData = {
+  userId: string;
+  sectionId: number;
+  score: number;
+};
